a8f83e28f27a9f7bd5a7026b09c105be8d6a00ec
Nguyễn Ngọc Bình <binhnguyen29122001@gmail.com>
2023-12-29 01:02:54 +0700

    add format message

--- src/app/health-tool/result/[resultId]/page.tsx
+++ src/app/health-tool/result/[resultId]/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { toolData } from '@/mock/mock-toolData';
-import { ICombineData } from '@/types/tool';
+import { ICombineData, formatText } from '@/types/tool';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import React, { useEffect, useState } from 'react';
@@ -26,11 +26,18 @@ export default function Result({}: IResultProps) {
     const [resultData, setResultData] = useState<ICombineData>();
     const [openTDEE, setOpenTDEE] = useState(false);
     const [openBRM, setOpenBRM] = useState(false);
+    const [message, setMessage] = useState({});
     const router = useRouter();
 
     useEffect(() => {
+        const categoryList = [
+            'Chế độ sinh hoạt',
+            'Chế độ dinh dưỡng',
+            'Phương pháp tập luyện',
+        ];
+        const formatedMessage = formatText(toolData.message, categoryList);
+        setMessage(formatedMessage);
         setResultData(toolData);
-
         return () => {};
     }, []);
 
@@ -150,15 +157,20 @@ export default function Result({}: IResultProps) {
                         in={openTDEE}
                         timeout="auto"
                         unmountOnExit
-                        sx={{ px: 5 }}
+                        sx={{ px: 5, pt: 2 }}
                     >
-                        <div className="text-gray-500 font-medium py-4">
+                        <div className="mb-5">
+                            <h4 className="text-boldGreen font-medium text-lg">
+                                TDEE là gì? Tính TDEE giảm cân lành mạnh
+                            </h4>
+                        </div>
+                        <div className="text-gray-400 font-normal text-base">
                             <p>
                                 TDEE (Total Daily Energy Expenditure) là tất cả
                                 năng lượng cần thiết cho hoạt động mỗi ngày của
                                 bạn. Tính TDEE giúp bạn đạt được mục tiêu dinh
                                 dưỡng (giảm cân, tăng cân) của bạn.
-                                <br />
+                                <div className="h-4"></div>
                                 Xác định chỉ số TDEE sẽ giúp bạn biết rõ lượng
                                 calo nên tăng hoặc giảm để đạt hiệu quả cutting
                                 (giảm cân) hay bulking (tăng cân). Nói cách
@@ -189,15 +201,99 @@ export default function Result({}: IResultProps) {
                         in={openBRM}
                         timeout="auto"
                         unmountOnExit
-                        sx={{ px: 5 }}
+                        sx={{ px: 5, pt: 2 }}
                     >
-                        <div className="text-gray-500 font-medium py-4">
-                            BRM là gì
+                        <div className="mb-5">
+                            <h4 className="text-boldGreen font-medium text-lg">
+                                BRM là gì?
+                            </h4>
+                        </div>
+                        <div className="text-gray-400 font-normal text-base">
+                            <p>
+                                Chỉ số BMR (Basal Metabolic Rate) là tỷ lệ trao
+                                đổi chất cơ bản trong cơ thể con người,cho biết
+                                mức năng lượng tối thiểu mà cơ thể cần, để thực
+                                hiện các chức năng cơ bản nhằm đảm bảo duy trì
+                                sự sống của cơ thể, khi bạn ở trạng thái nghỉ
+                                ngơi.
+                            </p>
                         </div>
                     </Collapse>
                 </List>
+                <div className="flex md:justify-between md:flex-col mb-10 mt-8 md:gap-4 px-4">
+                    <div className="flex md:flex-row md:items-end">
+                        <p className="text-boldGreen font-semibold text-lg">
+                            Lượng calo cần thiết cho ngày tập luyện là:
+                        </p>
+                        <div className="text-center mt-2 md: mx-4">
+                            <span className="text-yellowPrimary text-4xl font-bold">
+                                {resultData?.workoutDayCalo}
+                            </span>
+                        </div>
+                        <div className="mt-2 md:mt-0 md:mb-1">
+                            <span className="text-gray-400 text-sm md:leading-6">
+                                calories
+                            </span>
+                        </div>
+                    </div>
+                    <div className="flex md:flex-row md:items-end">
+                        <p className="text-boldGreen font-semibold text-lg">
+                            Lượng calo cần thiết cho ngày nghỉ là:
+                        </p>
+                        <div className="text-center mt-2 md: mx-4">
+                            <span className="text-yellowPrimary text-4xl font-bold">
+                                {resultData?.restDayCalo}
+                            </span>
+                        </div>
+                        <div className="mt-2 md:mt-0 md:mb-1">
+                            <span className="text-gray-400 text-sm md:leading-6">
+                                calories
+                            </span>
+                        </div>
+                    </div>
+                </div>
             </section>
-            <section className="my-14"></section>
+            <div className="max-w-[80%] border-b-1"></div>
+            <section className="my-14">
+                <h1 className="text-center font-semibold text-gray-700">
+                    Lời khuyên
+                </h1>
+                <div className="">
+                    {Object.keys(message).map((category) => (
+                        <div key={category} className="">
+                            <h5 className="">{category}</h5>
+                            <ul className="">
+                                {message[category].map(
+                                    (content, index: number) => (
+                                        <li key={index} className="">
+                                            {content}
+                                        </li>
+                                    ),
+                                )}
+                            </ul>
+                        </div>
+                    ))}
+
+                    {/* <div className="">
+                        <h5 className="">Chế độ sinh hoạt</h5>
+                        <ul className="">
+                            <li className=""></li>
+                        </ul>
+                    </div>
+                    <div className="">
+                        <h5 className="">Chế độ dinh dưỡng</h5>
+                        <ul className="">
+                            
+                        </ul>
+                    </div>
+                    <div className="">
+                        <h5 className="">Phương pháp tập luyện</h5>
+                        <ul className="">
+                            
+                        </ul>
+                    </div> */}
+                </div>
+            </section>
             <section className=""></section>
         </div>
     );
