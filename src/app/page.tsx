c71479e415426f247710be4e39069ee3572a6489
Nguyen Ngoc Binh <wize.trinh02@gmail.com>
2024-01-03 18:06:29 +0700

    Add intro app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,39 @@
-import * as React from 'react';
+'use client';
+
+import useSessionStorage from '@/hooks/useSessionStorage';
+import { useRouter } from 'next/navigation';
+import React, { useEffect } from 'react';
 
 export interface IAppProps {}
 
 export default function App(props: IAppProps) {
-    return <div></div>;
+    const isLogin = useSessionStorage('isLogin');
+    const router = useRouter();
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            console.log(isLogin);
+            if (isLogin) {
+                router.push('/home');
+            } else {
+                router.push('/auth');
+            }
+        }, 3800);
+
+        return () => {
+            clearTimeout(timeout);
+        };
+    }, []);
+
+    return (
+        <div className="center h-screen">
+            <video
+                src={require('../assets/video/medicalife.mp4')}
+                autoPlay
+                muted
+                loop
+                width="500"
+            ></video>
+        </div>
+    );
 }
